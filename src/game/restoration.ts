addade0b6a758e4c8b643eb0a4dce5bc0b3f18e0
ivangemini <ivangemini13@gmail.com>
2026-08-24 18:23:35 +0300

    Expose restoration strategy helpers

--- src/game/restoration.ts
+++ src/game/restoration.ts
@@ -1,7 +1,14 @@
 export {
+  RESTORATION_MODE_RULES,
   applyRestoration,
+  baseRestorationTargetHalfWidth,
   conditionValueMultiplier,
   estimateItemValue,
+  restorationTargetHalfWidth,
   roundToTen,
 } from '../domain/restoration';
-export type { RestorationOutcome } from '../domain/restoration';
+export type {
+  RestorationMode,
+  RestorationModeRules,
+  RestorationOutcome,
+} from '../domain/restoration';
