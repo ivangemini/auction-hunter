9ebbd4db15f35464a1dbfe7b0046ebe075579fcd
ivangemini <ivangemini13@gmail.com>
2026-08-26 10:20:15 +0300

    Register third P9 catalog batch in tests

--- src/data/testSetup.ts
+++ src/data/testSetup.ts
@@ -1,4 +1,5 @@
 import { registerCampaignBreadth } from './campaignBreadth';
+import { registerCampaignBreadthThree } from './campaignBreadthThree';
 import { registerCampaignBreadthTwo } from './campaignBreadthTwo';
 import { registerCampaignDiscoveryChains } from './campaignDiscoveryChains';
 import { registerNadiaCampaignArc } from './campaignPrincipalArc';
@@ -7,5 +8,6 @@ import { registerItemBreadth } from './itemBreadth';
 registerItemBreadth();
 registerCampaignBreadth();
 registerCampaignBreadthTwo();
+registerCampaignBreadthThree();
 registerNadiaCampaignArc();
 registerCampaignDiscoveryChains();
