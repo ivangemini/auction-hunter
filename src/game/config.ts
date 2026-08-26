3c7d04a3d91bb388d87c34e92dafda0b5f8f909d
ivangemini <ivangemini13@gmail.com>
2026-08-26 10:15:49 +0300

    Register campaign-enabled Office headquarters

--- src/game/config.ts
+++ src/game/config.ts
@@ -1,13 +1,13 @@
 import Phaser from 'phaser';
 import { CampaignFinaleScene } from './scenes/CampaignFinaleScene';
 import { CampaignInboxScene } from './scenes/CampaignInboxScene';
+import { CampaignOfficeScene } from './scenes/CampaignOfficeScene';
 import { CampaignOptionalScene } from './scenes/CampaignOptionalScene';
 import { CampaignPrincipalScene } from './scenes/CampaignPrincipalScene';
 import { CampaignProvenanceAuctionScene } from './scenes/CampaignProvenanceAuctionScene';
 import { CollectionScene } from './scenes/CollectionScene';
 import { CollectorRequestBuyerMarketScene } from './scenes/CollectorRequestBuyerMarketScene';
 import { DiscoveryBoardScene } from './scenes/DiscoveryBoardScene';
-import { OfficeScene } from './scenes/OfficeScene';
 import { OnboardingScene } from './scenes/OnboardingScene';
 
 export const gameConfig: Phaser.Types.Core.GameConfig = {
@@ -16,7 +16,7 @@ export const gameConfig: Phaser.Types.Core.GameConfig = {
   width: 1280,
   height: 720,
   backgroundColor: '#101216',
-  scene: [OnboardingScene, CampaignPrincipalScene, CampaignInboxScene, CampaignOptionalScene, CampaignFinaleScene, CampaignProvenanceAuctionScene, CollectionScene, DiscoveryBoardScene, CollectorRequestBuyerMarketScene, OfficeScene],
+  scene: [OnboardingScene, CampaignPrincipalScene, CampaignInboxScene, CampaignOptionalScene, CampaignFinaleScene, CampaignProvenanceAuctionScene, CollectionScene, DiscoveryBoardScene, CollectorRequestBuyerMarketScene, CampaignOfficeScene],
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
