122d13b12f640984cba55f08c1b9af7451a667ed
ivangemini <ivangemini13@gmail.com>
2026-08-26 10:03:25 +0300

    Register campaign discovery cases in tests

--- src/data/testSetup.ts
+++ src/data/testSetup.ts
@@ -1,7 +1,9 @@
 import { registerCampaignBreadth } from './campaignBreadth';
 import { registerCampaignBreadthTwo } from './campaignBreadthTwo';
+import { registerCampaignDiscoveryChains } from './campaignDiscoveryChains';
 import { registerItemBreadth } from './itemBreadth';
 
 registerItemBreadth();
 registerCampaignBreadth();
 registerCampaignBreadthTwo();
+registerCampaignDiscoveryChains();
