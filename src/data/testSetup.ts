707c30ebb8bd9528979d8d89f33b01b5b91b4ebc
ivangemini <ivangemini13@gmail.com>
2026-08-26 10:09:41 +0300

    Register Nadia arc in tests

--- src/data/testSetup.ts
+++ src/data/testSetup.ts
@@ -1,9 +1,11 @@
 import { registerCampaignBreadth } from './campaignBreadth';
 import { registerCampaignBreadthTwo } from './campaignBreadthTwo';
 import { registerCampaignDiscoveryChains } from './campaignDiscoveryChains';
+import { registerNadiaCampaignArc } from './campaignPrincipalArc';
 import { registerItemBreadth } from './itemBreadth';
 
 registerItemBreadth();
 registerCampaignBreadth();
 registerCampaignBreadthTwo();
+registerNadiaCampaignArc();
 registerCampaignDiscoveryChains();
